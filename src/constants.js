531944f9ffbf209dfd6e43f7d938d12adb1516c6
Tanner B. Hess Webber <tb@hesswebber.com>
2023-02-08 02:41:28 +0000

    style(queries): sets standards for media queries

--- src/constants.js
+++ src/constants.js
@@ -18,7 +18,13 @@ export const WEIGHTS = {
 };
 
 export const BREAKPOINTS = {
-  phone: "37.5rem",
-  tablet: "59.375rem",
-  desktop: "81.25rem",
+  phone: 600,
+  tablet: 950,
+  laptop: 1300,
+};
+
+export const QUERIES = {
+  phoneAndDown: `max-width: ${BREAKPOINTS.phone / 16}rem`,
+  tabletAndDown: `max-width: ${BREAKPOINTS.tablet / 16}rem`,
+  laptopAndDown: `max-width: ${BREAKPOINTS.laptop / 16}rem`,
 };
